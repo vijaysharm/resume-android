ed7baaf8d513dc2d1cb18e9802eac25834e08d77
Vijay Sharma <vijay.sharm@gmail.com>
2015-02-28 22:53:35 -0500

    made status and actiobar translucent

--- app/src/main/java/ca/vijaysharma/resume/ResumeActivity.java
+++ app/src/main/java/ca/vijaysharma/resume/ResumeActivity.java
@@ -1,19 +1,23 @@
 package ca.vijaysharma.resume;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowInsets;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 
 public class ResumeActivity extends Activity {
+    @InjectView(R.id.container) ViewGroup container;
     @InjectView(R.id.me) ViewPager profile;
     @InjectView(R.id.experience) ViewPager experience;
     @InjectView(R.id.skills) ViewPager skills;
@@ -24,6 +28,15 @@ public class ResumeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume);
         ButterKnife.inject(this);
+
+        final TypedArray styledAttributes = getTheme().obtainStyledAttributes(
+            new int[] { android.R.attr.actionBarSize });
+        int toolbarHeight = (int) styledAttributes.getDimension(0, 0);
+        styledAttributes.recycle();
+
+        getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getActionBar().setCustomView(R.layout.action_bar);
+        applyInsets(toolbarHeight);
 
         profile.setAdapter(new ProfileAdapter(this));
         preparePager(profile);
@@ -36,6 +49,17 @@ public class ResumeActivity extends Activity {
 
         skills.setAdapter(new ProfileAdapter(this));
         preparePager(skills);
+    }
+
+    private void applyInsets(final int toolbarHeight) {
+        container.setFitsSystemWindows(true);
+        container.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
+            @Override
+            public WindowInsets onApplyWindowInsets(View view, WindowInsets insets) {
+                view.setPadding(0, toolbarHeight, 0, insets.getSystemWindowInsetBottom());
+                return insets.consumeSystemWindowInsets();
+            }
+        });
     }
 
     private void preparePager(ViewPager pager) {
@@ -54,7 +78,7 @@ public class ResumeActivity extends Activity {
 
         @Override
         public int getCount() {
-            return 10;
+            return 2;
         }
 
         @Override
